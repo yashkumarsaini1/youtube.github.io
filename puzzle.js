13ce893a2b52585dde8b6f561049f7619182c30c
yashkumarsaini1 <61925060+yashkumarsaini1@users.noreply.github.com>
2020-05-02 09:25:16 +0530

    Add files via upload

---
+++ puzzle.js
@@ -0,0 +1,284 @@
+	var count=0;
+
+	function block1()
+	{   
+		 var	x1 = document.getElementById("box1").value;
+	var	x2 = document.getElementById("box2").value;
+	var	x3 = document.getElementById("box3").value;
+   	var	x4 = document.getElementById("box4").value;
+	var	x5 = document.getElementById("box5").value;
+   	var	x6 = document.getElementById("box6").value;
+	var	x7 = document.getElementById("box7").value;
+	var	x8 = document.getElementById("box8").value;
+	var	x9 = document.getElementById("box9").value;
+		
+		if(x4==" ")
+		{
+			document.getElementById("box4").value = document.getElementById("box1").value;
+			document.getElementById("box1").value = " ";
+		}
+		else if(x2==" ")
+		{
+			document.getElementById("box2").value = document.getElementById("box1").value;
+			document.getElementById("box1").value = " ";
+		}
+		check();
+		count++;
+	}
+
+	function block2()
+	{ var	x1 = document.getElementById("box1").value;
+	var	x2 = document.getElementById("box2").value;
+	var	x3 = document.getElementById("box3").value;
+   	var	x4 = document.getElementById("box4").value;
+	var	x5 = document.getElementById("box5").value;
+   	var	x6 = document.getElementById("box6").value;
+	var	x7 = document.getElementById("box7").value;
+	var	x8 = document.getElementById("box8").value;
+	var	x9 = document.getElementById("box9").value;
+		
+
+		if(x1==" ")
+		{
+			document.getElementById("box1").value = document.getElementById("box2").value;
+			document.getElementById("box2").value = " ";
+		}
+		else if(x5==" ")
+		{
+			document.getElementById("box5").value = document.getElementById("box2").value;
+			document.getElementById("box2").value = " ";
+			
+		}
+		else if(x3==" ")
+		{
+			document.getElementById("box3").value = document.getElementById("box2").value;
+			document.getElementById("box2").value = " ";
+		}
+		check();
+		count++;
+	}
+
+	function block3()
+	{ var	x1 = document.getElementById("box1").value;
+	var	x2 = document.getElementById("box2").value;
+	var	x3 = document.getElementById("box3").value;
+   	var	x4 = document.getElementById("box4").value;
+	var	x5 = document.getElementById("box5").value;
+   	var	x6 = document.getElementById("box6").value;
+	var	x7 = document.getElementById("box7").value;
+	var	x8 = document.getElementById("box8").value;
+	var	x9 = document.getElementById("box9").value;
+		if(x6==" ")
+		{
+			document.getElementById("box6").value = document.getElementById("box3").value;
+			document.getElementById("box3").value = " ";
+		}
+		else if(x2==" ")
+		{
+			document.getElementById("box2").value = document.getElementById("box3").value;
+			document.getElementById("box3").value = " ";
+		}
+		check();
+		count++;
+	}
+
+	function block4()
+	{ var	x1 = document.getElementById("box1").value;
+	var	x2 = document.getElementById("box2").value;
+	var	x3 = document.getElementById("box3").value;
+   	var	x4 = document.getElementById("box4").value;
+	var	x5 = document.getElementById("box5").value;
+   	var	x6 = document.getElementById("box6").value;
+	var	x7 = document.getElementById("box7").value;
+	var	x8 = document.getElementById("box8").value;
+	var	x9 = document.getElementById("box9").value;
+		if(x1==" ")
+		{
+			document.getElementById("box1").value = document.getElementById("box4").value;
+			document.getElementById("box4").value = " ";
+		}
+		else if(x5==" ")
+		{
+			document.getElementById("box5").value = document.getElementById("box4").value;
+			document.getElementById("box4").value = " ";
+		
+		}
+		else if(x7==" ")
+		{
+			document.getElementById("box7").value = document.getElementById("box4").value;
+			document.getElementById("box4").value = " ";
+		
+		}
+		check();
+		count++;
+	}
+
+	function block5()
+	{ var	x1 = document.getElementById("box1").value;
+	var	x2 = document.getElementById("box2").value;
+	var	x3 = document.getElementById("box3").value;
+   	var	x4 = document.getElementById("box4").value;
+	var	x5 = document.getElementById("box5").value;
+   	var	x6 = document.getElementById("box6").value;
+	var	x7 = document.getElementById("box7").value;
+	var	x8 = document.getElementById("box8").value;
+	var	x9 = document.getElementById("box9").value;
+		if(x4==" ")
+		{
+			document.getElementById("box4").value = document.getElementById("box5").value;
+			document.getElementById("box5").value = " ";
+		
+		}
+		else if(x2==" ")
+		{
+			document.getElementById("box2").value = document.getElementById("box5").value;
+			document.getElementById("box5").value = " ";
+		}
+
+		else if(x8==" ")
+		{
+			document.getElementById("box8").value = document.getElementById("box5").value;
+			document.getElementById("box5").value = " ";
+		}
+		else if(x6==" ")
+		{
+			document.getElementById("box6").value = document.getElementById("box5").value;
+			document.getElementById("box5").value = " ";
+		}
+		check();
+		count++;
+	}
+
+	function block6()
+	{ var	x1 = document.getElementById("box1").value;
+	var	x2 = document.getElementById("box2").value;
+	var	x3 = document.getElementById("box3").value;
+   	var	x4 = document.getElementById("box4").value;
+	var	x5 = document.getElementById("box5").value;
+   	var	x6 = document.getElementById("box6").value;
+	var	x7 = document.getElementById("box7").value;
+	var	x8 = document.getElementById("box8").value;
+	var	x9 = document.getElementById("box9").value;
+
+		if(x9==" ")
+		{
+			document.getElementById("box9").value = document.getElementById("box6").value;
+			document.getElementById("box6").value = " ";
+		}
+		else if(x5==" ")
+		{
+			document.getElementById("box5").value = document.getElementById("box6").value;
+			document.getElementById("box6").value = " ";
+		}
+		else if(x3==" ")
+		{
+			document.getElementById("box3").value = document.getElementById("box6").value;
+			document.getElementById("box6").value = " ";
+		}
+		check();
+		count++;
+	}
+
+	function block7()
+	{ var	x1 = document.getElementById("box1").value;
+	var	x2 = document.getElementById("box2").value;
+	var	x3 = document.getElementById("box3").value;
+   	var	x4 = document.getElementById("box4").value;
+	var	x5 = document.getElementById("box5").value;
+   	var	x6 = document.getElementById("box6").value;
+	var	x7 = document.getElementById("box7").value;
+	var	x8 = document.getElementById("box8").value;
+	var	x9 = document.getElementById("box9").value;
+		if(x8==" ")
+		{
+			document.getElementById("box8").value = document.getElementById("box7").value;
+			document.getElementById("box7").value = " ";
+		}
+		else if(x4==" ")
+		{
+			document.getElementById("box4").value = document.getElementById("box7").value;
+			document.getElementById("box7").value = " ";
+		}
+		check();
+		count++;
+
+	}
+
+	function block8()
+	{ var	x1 = document.getElementById("box1").value;
+	var	x2 = document.getElementById("box2").value;
+	var	x3 = document.getElementById("box3").value;
+   	var	x4 = document.getElementById("box4").value;
+	var	x5 = document.getElementById("box5").value;
+   	var	x6 = document.getElementById("box6").value;
+	var	x7 = document.getElementById("box7").value;
+	var	x8 = document.getElementById("box8").value;
+	var	x9 = document.getElementById("box9").value;
+
+		if(x9==" ")
+		{
+			document.getElementById("box9").value = document.getElementById("box8").value;
+			document.getElementById("box8").value = " ";
+		}
+		else if(x5==" ")
+		{
+			document.getElementById("box5").value = document.getElementById("box8").value;
+			document.getElementById("box8").value = " ";
+		}
+		else if(x7==" ")
+		{
+			document.getElementById("box7").value = document.getElementById("box8").value;
+			document.getElementById("box8").value = " ";
+		}
+		check();
+		count++;
+		
+	}
+	function block9()
+	{ var	x1 = document.getElementById("box1").value;
+	var	x2 = document.getElementById("box2").value;
+	var	x3 = document.getElementById("box3").value;
+   	var	x4 = document.getElementById("box4").value;
+	var	x5 = document.getElementById("box5").value;
+   	var	x6 = document.getElementById("box6").value;
+	var	x7 = document.getElementById("box7").value;
+	var	x8 = document.getElementById("box8").value;
+	var	x9 = document.getElementById("box9").value;
+		if(x8==" ")
+		{
+			document.getElementById("box8").value = document.getElementById("box9").value;
+			document.getElementById("box9").value = " ";
+		}
+		else if(x6==" ")
+		{
+			document.getElementById("box6").value = document.getElementById("box9").value;
+			document.getElementById("box9").value = " ";
+		}
+		check();
+		count++;
+	}
+
+function check(){
+	var x1 = document.getElementById("box1").value;
+	var	x2 = document.getElementById("box2").value;
+	var	x3 = document.getElementById("box3").value;
+   	var	x4 = document.getElementById("box4").value;
+	var	x5 = document.getElementById("box5").value;
+   	var	x6 = document.getElementById("box6").value;
+	var	x7 = document.getElementById("box7").value;
+	var	x8 = document.getElementById("box8").value;
+	var	x9 = document.getElementById("box9").value;
+
+if ((x1=="1")&&(x2=="2")&&(x3=="3")&&(x4=="4")&&(x5=="5")&&(x6=="6")&&(x7=="7")&&(x8=="8")) 
+{
+	document.getElementById("result").innerHTML ="You have completed this puzzle"+"<br/>"+"Steps:"+count+
+	"<br/>"+"Your Score="+(200-count);
+
+}
+else
+{
+	document.getElementById("result").innerHTML ="You can solove it";
+}
+}
+
+
